6716cf7227d7a23528b43a4349f52b81f9921340
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2022-08-08 23:24:44 +0600

    loader added

--- src/Pages/Loading/Loading.js
+++ src/Pages/Loading/Loading.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
-const Loading = () => {
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+const Loading = (props) => {
     return (
         <div>
-            <p>loading</p>
+            <h1>{props?.title || <Skeleton />}</h1>
+            {props?.body || <Skeleton count={10} />}
         </div>
     );
 };
